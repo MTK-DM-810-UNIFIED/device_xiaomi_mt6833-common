67cdbfa380fc29df92696baf54bbbd6c0e6c1cb0
Taqi Raza <mttalpur@gmail.com>
2023-10-30 02:52:19 +0500

    mt6833-common: add support for miui camera vendor

--- mt6833.mk
+++ mt6833.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/mt6833-common/mt6833-common-vendor.mk)
 
+# Inherit MIUI Camera Vendor
+$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
+
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 

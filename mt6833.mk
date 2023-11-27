06de85088e7ac3079b33fb8bb6ba3724d2c06489
Taqi Raza <mttalpur@gmail.com>
2023-11-27 23:58:51 +0500

    mt6833-common: add missing `android.hardware.power@1.0`
    *required by
    
    vendor/lib64/libcam.hal3a.v3.platform.so
    vendor/lib64/libcam.halisp.common.so
    vendor/lib64/libmtkcam_sysutils.so
    vendor/lib64/libcam.isptuning.so
    vendor/lib64/libcam.halisp.so
    vendor/lib64/libmtkcam_pipelinemodel_utils.so
    vendor/lib64/libmtkcam_hwutils.so
    vendor/lib64/vendor.mediatek.hardware.power@2.0.so

--- mt6833.mk
+++ mt6833.mk
@@ -304,6 +304,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-mediatek \
+    android.hardware.power@1.0.vendor \
     android.hardware.power@1.2.vendor \
     vendor.mediatek.hardware.mtkpower@1.2.vendor
 

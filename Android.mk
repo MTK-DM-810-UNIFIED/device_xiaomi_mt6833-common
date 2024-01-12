ad16a0c226ecc56af91b68ecf1533c1d0d374b5f
Taqi Raza <mttalpur@gmail.com>
2024-01-13 01:26:33 +0500

    mt6833-common: fixup vendor symlinks

--- Android.mk
+++ Android.mk
@@ -12,12 +12,20 @@ subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
 $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
 VENDOR_SYMLINKS := \
-	$(TARGET_OUT_VENDOR)/etc
+	$(TARGET_OUT_VENDOR)/etc \
+	$(TARGET_OUT_VENDOR)/lib/hw \
+	$(TARGET_OUT_VENDOR)/lib64/hw
 
 $(VENDOR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) echo "Making vendor symlinks"
 	@mkdir -p $(TARGET_OUT_VENDOR)/etc
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
+	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
 	@ln -sf /vendor/etc/audio_param $(TARGET_OUT_VENDOR)/etc/audio_param_global
+	@ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
+	@ln -sf /vendor/lib64/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
 	$(hide) touch $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SYMLINKS)

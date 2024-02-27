c0ed56f60e024fa045ecce0fc507465cd2fa8214
Taqi Raza <mttalpur@gmail.com>
2024-02-27 21:02:33 +0500

    mt6833-common: fixup gnss depend for S

--- mt6833.mk
+++ mt6833.mk
@@ -155,7 +155,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor:64 \
     android.hardware.gnss.visibility_control@1.0.vendor:64 \
     android.hardware.gnss@2.1.vendor:64 \
-    android.hardware.gnss-V1-ndk.vendor:64
+    android.hardware.gnss-V1-ndk_platform.vendor:64
 
 # Gatekeeper
 PRODUCT_PACKAGES += \

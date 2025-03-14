6ba606e0fad7d55d5118ff6e7aa9f43045be93cb
aleeeee1 <pace.alessandro05@protonmail.com>
2025-03-14 05:53:03 +0530

    mt6833-common;  add right namespaces to power-mode
    Fixes
    
    Output:
    ld.lld: error: undefined symbol: _ZN4aidl6google8hardware5power4impl5pixel21setDeviceSpecificModeENS_7android8hardware5power4ModeEb
    >>> referenced by Power.cpp:88 (hardware/xiaomi/aidl/power-libperfmgr/Power.cpp:88)

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -1,17 +1,7 @@
 /*
- * Copyright (C) 2020 The LineageOS Project
+ * Copyright (C) 2024 The LineageOS Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <aidl/android/hardware/power/BnPower.h>
@@ -55,11 +45,11 @@ int open_ts_input() {
 }  // anonymous namespace
 
 namespace aidl {
-namespace android {
+namespace google {
 namespace hardware {
 namespace power {
 namespace impl {
-namespace mediatek {
+namespace pixel {
 
 static constexpr int kInputEventWakeupModeOff = 4;
 static constexpr int kInputEventWakeupModeOn = 5;
@@ -98,9 +88,9 @@ bool setDeviceSpecificMode(Mode type, bool enabled) {
     }
 }
 
-}  // namespace mediatek
+}  // namespace pixel
 }  // namespace impl
 }  // namespace power
 }  // namespace hardware
-}  // namespace android
+}  // namespace google
 }  // namespace aidl

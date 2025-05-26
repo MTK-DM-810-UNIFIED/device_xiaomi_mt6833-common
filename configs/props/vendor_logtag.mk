526883e78a3da1cb650a636629b76e599b547eaa
ChrisCatto <chriscatto@proton.me>
2025-05-26 23:08:49 +0530

    mt6833-common: Silence HWUI logspam
    * After Android 15 QPR2, HWUI keeps spamming "Image decoding logging
      dropped!" so mute it since any other information from it is not
      really any more useful.

--- configs/props/vendor_logtag.mk
+++ configs/props/vendor_logtag.mk
@@ -258,4 +258,6 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.PowerHalMgrServiceImpl=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.PowerHalWifiMonitor=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.gralloc4=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.OpenGLRenderer=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.OpenGLRenderer=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.GPUAUX=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.HWUI=$(VENDOR_LOG_LEVEL)

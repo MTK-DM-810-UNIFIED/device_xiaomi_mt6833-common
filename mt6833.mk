cadc1863fe523449bb2da852685fd53496b1e075
Sushrut1101 <guptasushrut@gmail.com>
2025-06-19 22:17:45 +0530

    mt6833-common: Import `libmtkperf_client` namespace
    Change-Id: I9302f622b085415d75bab0a1ebc6a27d8d0e00cf
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -388,6 +388,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/mediatek \
+    hardware/mediatek/libmtkperf_client \
     hardware/xiaomi
 
 # Thermal
